4c0c5bb3407f3c394a988a8ced66cc4a69c671c5
emisua <10.khima@gmail.com>
2022-05-10 12:41:34 +0200

    Quito el router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
-root.render(
-  <BrowserRouter basename={window.location.pathname || ''}>
-    <Route exact path='/' component={App} />
-  </BrowserRouter>
-)
+root.render(<App />)
