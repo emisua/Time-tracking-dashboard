2df489f7599eec962c1b621991cd809d256d541c
emisua <10.khima@gmail.com>
2022-05-10 12:21:27 +0200

    añadido router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+import App from './App'
+import './index.css'
+
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+)
